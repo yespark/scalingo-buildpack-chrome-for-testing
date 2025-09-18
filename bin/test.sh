89519bb13923bce0f788ca7487e5f22450b852c1
Navid EMAD <navid.emad@yespark.fr>
2025-09-18 11:27:28 +0200

    migration

--- bin/test.sh
+++ bin/test.sh
@@ -6,20 +6,17 @@ STACK_VERSION="${1:?'Error: The stack version number must be specified as the fi
 
 set -x
 
-docker build --progress=plain --build-arg="STACK_VERSION=${STACK_VERSION}" -t heroku-buildpack-chrome-for-testing .
+docker build --platform=linux/amd64 --progress=plain --build-arg="STACK_VERSION=${STACK_VERSION}" -t scalingo-buildpack-chrome-for-testing .
 
 # Note: All of the container commands must be run via a login bash shell otherwise the profile.d scripts won't be run.
 
-# Check the profile.d scripts correctly added the binaries to PATH.
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chrome --version'
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chromedriver --version'
+# # Check the profile.d scripts correctly added the binaries to PATH.
+docker run --platform=linux/amd64 --rm scalingo-buildpack-chrome-for-testing bash -l -c 'chrome --version'
+docker run --platform=linux/amd64 --rm scalingo-buildpack-chrome-for-testing bash -l -c 'chromedriver --version'
 
-# Check that there are no missing dynamically linked libraries.
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chrome)'
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chromedriver)'
+# # Check that there are no missing dynamically linked libraries.
+docker run --platform=linux/amd64 --rm scalingo-buildpack-chrome-for-testing bash -l -c 'ldd $(which chrome)'
+docker run --platform=linux/amd64 --rm scalingo-buildpack-chrome-for-testing bash -l -c 'ldd $(which chromedriver)'
 
-# Check Chrome can fully boot.
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chrome --no-sandbox --headless --screenshot https://google.com'
-
-# Display a size breakdown of the directories added by the buildpack to the app.
-docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'du --human-readable --max-depth=1 /app'
+# # Display a size breakdown of the directories added by the buildpack to the app.
+docker run --platform=linux/amd64 --rm scalingo-buildpack-chrome-for-testing bash -l -c 'du --human-readable --max-depth=1 /app'
